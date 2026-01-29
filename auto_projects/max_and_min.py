fc7c4fcccbe94f099bff46c7fb83ad1f17b143b8
d4rkonus <d4rkonus@gmail.com>
2026-01-29 13:33:45 +0000

    Update max_and_min.py

--- auto_projects/max_and_min.py
+++ auto_projects/max_and_min.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 
-numbers = []
+while True:
+    try:
+        numbers = []
+        number = input("\nGive me the numbers: ")
 
-number = input("\nGive me the numbers: ")
+        for number in number.split():
+            numbers.append(int(number))
 
-for number in number.split():
-    numbers.append(int(number))
+        max = numbers[0]
+        min = numbers[0]
 
-max = numbers[0]
-min = numbers[0]
+        for number in numbers:
+            if number > max:
+                max = number
+            if number < min:
+                min = number
 
-for number in numbers:
-    if number > max:
-        max = number
-    if number < min:
-        min = number
+    except KeyboardInterrupt:
+        print("\n\n[!] Moving to safe zone...")
+        break
+    
+    unique_numbers = list(set(numbers))
 
-unique_numbers = list(set(numbers))
-
-print(f"\nThe biggest number is: {max}")
-print(f"\nThe smallest number is {min}")
+    print(f"\nThe biggest number is: {max}")
+    print(f"\nThe smallest number is {min}")
