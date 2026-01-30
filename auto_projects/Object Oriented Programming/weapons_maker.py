ca14204b99215cd84329078c53f441d6db490a5d
d4rkonus <d4rkonus@gmail.com>
2026-01-30 11:22:21 +0000

    Update weapons_maker.py

--- auto_projects/Object Oriented Programming/weapons_maker.py
+++ auto_projects/Object Oriented Programming/weapons_maker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import time
 
@@ -16,59 +16,52 @@ class Warrior:
         self.strength = strength
         self.weapon = weapon
 
- 
-# --- Weapon Instances ---
+
+# --- Weapons ---
 axe = Weapon("axe", "ice", 50)
 hammer = Weapon("hammer", "lightning", 80)
 sword = Weapon("sword", "fire", 70)
 spear = Weapon("spear", "air", 40)
 globes = Weapon("globes", "rock", 45)
 
-
-# --- Warriors Instances ---
-lancelot = Warrior("lancelot", 70, spear)
-surtr = Warrior("surtr", 90, sword)
-bercox = Warrior("bercox", 60, axe)
-navinni = Warrior("navinni", 50, globes)
-nexus = Warrior("nexus", 80, hammer)
-
-weapons = [axe, hammer, sword, spear, globes]
-warriors = [lancelot, surtr, bercox, navinni, nexus]
+# --- Warriors ---
+warriors = [
+    Warrior("lancelot", 70, spear),
+    Warrior("surtr", 90, sword),
+    Warrior("bercox", 60, axe),
+    Warrior("navinni", 50, globes),
+    Warrior("nexus", 80, hammer),
+]
 
 
-def warriors_weapons():
+def warriors_weapons(round_number, counter, max_counts):
     for warrior in warriors:
-        if count == max_counts:
-            break
-        time.sleep(1)
-        print(f"\n[+] The warrior {warrior.name} has the {warrior.weapon.model} weapon with {warrior.weapon.power} power and {warrior.weapon.damage} damage.")
-        count += 1
-        time.sleep(1)
-    return count
-        
+        if counter == max_counts:
+            return counter
 
-count = 0
-max_counts = 5
-  
-def warriors_weapons(round_number, counter):
-    for warrior in warriors:
-            counter += 1
-            time.sleep(1)
-            print(
-                f"\n[{counter}] Round {round_number}: The warrior {warrior.name} has the "
-                f"{warrior.weapon.model} weapon with {warrior.weapon.power} power and "
-                f"{warrior.weapon.damage} damage."
-            )
-            time.sleep(1)
+        counter += 1
+        time.sleep(1)
+        print(
+            f"\n[{counter}] Round {round_number}: "
+            f"The warrior {warrior.name} has the "
+            f"{warrior.weapon.model} weapon with "
+            f"{warrior.weapon.power} power and "
+            f"{warrior.weapon.damage} damage."
+        )
+        time.sleep(1)
+
     return counter
 
 
-max_loops = 5
+MAX_ROUNDS = 5
+MAX_COUNTS = 5
 
 try:
     counter = 0
-    for round_number in range(1, max_loops + 1):
-        counter = warriors_weapons(round_number, counter)
+    for round_number in range(1, MAX_ROUNDS + 1):
+        counter = warriors_weapons(round_number, counter, MAX_COUNTS)
+        if counter == MAX_COUNTS:
+            break
 
 except KeyboardInterrupt:
     print("\n\n[!] Moving to safe zone ...")
