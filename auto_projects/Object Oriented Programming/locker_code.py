be7e873734999619c453b0b6fefd4a38ccb92870
d4rkonus <d4rkonus@gmail.com>
2026-02-01 11:52:43 +0000

    Update locker_code.py

--- auto_projects/Object Oriented Programming/locker_code.py
+++ auto_projects/Object Oriented Programming/locker_code.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+
 class Locker:
     def __init__(self, locker_number, locker_code=4320):
         self.locker_number = locker_number
         self.locker_code = locker_code
+        self.is_open = False
 
     def open_locker(self, input_code):
         if input_code == self.locker_code:
-            return f"\nLocker {self.locker_number} is now open."
-        else:
-            return "\nIncorrect code. Access denied."
+            self.is_open = True
+            return True
+        return False
         
 locker_number = input("Enter locker number: ")
 locker = Locker(locker_number)
@@ -17,9 +19,16 @@ locker = Locker(locker_number)
 while True:
     try:
         input_code = int(input("Enter locker code to open: "))
-        print(locker.open_locker(input_code))
-    except ValueError:
-        print("Please enter a valid numeric code.")
+        if locker.open_locker(input_code):
+            print(f"\nLocker {locker.locker_number} is now open!")
+            break
+        else:
+            print("\nIncorrect code. Try again.")
+        
     except KeyboardInterrupt:
-        print("\n\nExiting...")
+        print("\n\n[!] Moving to safe zone ...")
         break
+
+    except ValueError:
+        print("\n[!] Please enter a valid numeric code.")
+
